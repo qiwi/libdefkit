2d9586693dea45c4c2043533c88c266fc767571c
Anton Golub <antongolub@antongolub.com>
2022-01-30 22:07:18 +0300

    feat!: make target/es6 default package entry

--- src/main/ts/libdefkit.ts
+++ src/main/ts/libdefkit.ts
@@ -13,7 +13,7 @@ export const normalize: IExecPipe = (flags: ICliFlags): IContext => {
   const cwd = flags.cwd || process.cwd()
   const cache = findCacheDir({ name: '@qiwi/libdefkit' }) + ''
   const name = fs.readJsonSync(join(cwd, 'package.json')).name
-  const entry = flags.entry ?? `${name}/target/es5`
+  const entry = flags.entry ?? `${name}/target/es6`
   const dtsOut = flags.dtsOut ?? join(cwd, 'typings', 'index.d.ts')
   const flowOut = flags.flowOut ?? join(cwd, 'flow-typed', 'index.flow.js')
 
